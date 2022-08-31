160c1efbd74f5507bf0cdb4d711bba74eee8ef2b
Paweł Urbańczyk <pawel.urbanczyk@bpol.net>
2022-08-31 19:30:14 +0200

    processing status

--- src/Payum/Status/SyliusGetStatus.php
+++ src/Payum/Status/SyliusGetStatus.php
@@ -5,14 +5,15 @@ declare(strict_types=1);
 
 namespace BPolNet\SyliusSmart2PayPlugin\Payum\Status;
 
+use Payum\Core\Request\GetStatusInterface;
 use Sylius\Bundle\PayumBundle\Request\GetStatus;
+use Sylius\Component\Payment\Model\PaymentInterface;
 
 class SyliusGetStatus extends GetStatus
 {
-    public const STATE_AWAITING_CONFIRMATION = 'Awaiting for payment confirmation';
 
     public function markAwaitingConfirmation()
     {
-        $this->status = self::STATE_AWAITING_CONFIRMATION;
+        $this->status = PaymentInterface::STATE_PROCESSING;
     }
 }
